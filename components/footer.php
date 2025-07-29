568b948f644efd15f3c0033e9ec6c864dbcaa1aa
Bernard Almeida da Costa <biernard@gmail.com>
2025-07-29 16:10:32 +0200

    updated spelling issues

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,6 @@
 <footer class="w-full h-auto bg-gray-800 text-center p-3 text-sm z-10 opacity-75">
     <span class="text-shadow-lg text-orange-400">
 
-        &copy; <?= date('Y') ?> Pokédex com PHP + Tailwind
+        &copy; <?= date('Y') ?> Pokedex with PHP + Tailwind
     </span>
 </footer>
--- components/header.php
+++ components/header.php
@@ -10,6 +10,6 @@
 
 <body class="bg-gray-100 text-gray-800">
     <header class="bg-gray-300 bg-opacity-50 shadow-[0_6px_10px_-4px_rgba(0,0,0,0.2)] text-black p-4 text-center text-2xl font-bold fixed place-content-center w-full z-10">
-        Pokédex PHP
+        Pokedex PHP
     </header>
     <main class="container mx-auto p-4">
