54139ee31750484cc386bfe7ac2f61fbbbf8dab7
Bernard Almeida da Costa <biernard@gmail.com>
2025-07-31 14:36:47 +0200

    added a button in the logo that goes to index.php and fixed the footer layout

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,5 @@
-<footer class="w-full h-auto bg-gray-300 bg-opacity-50 text-center p-3 text-sm z-10 opacity-75">
+<footer class="bg-gray-300 bg-opacity-50 shadow-[0_6px_10px_-4px_rgba(0,0,0,0.1)] text-center p-3 text-sm z-10 opacity-75">
     <span class="text-shadow-lg">
-
         &copy; <?= date('Y') ?> Pokedex with PHP + Tailwind
     </span>
 </footer>
--- components/header.php
+++ components/header.php
@@ -9,12 +9,15 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-100">
+<body class="bg-gray-100 min-h-screen flex flex-col">
     <header class="bg-gray-300 bg-opacity-50 shadow-[0_6px_10px_-4px_rgba(0,0,0,0.2)] p-4 fixed w-full z-10">
         <div class="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
             <div class="flex items-center gap-2 opacity-70 hover:opacity-100 transition-opacity">
                 <h1 class="text-2xl font-bold text-center md:text-left w-full md:w-auto ">Pokedex PHP</h1>
-                <img src="assets/php.svg" alt="Logo" class="w-10 h-10" />
+                <a href="index.php">
+
+                    <img src="assets/php.svg" alt="Logo" class="w-10 h-10" />
+                </a>
             </div>
             <form method="GET" class="sm:w-1/4 md:w-1/3">
                 <input
@@ -42,4 +45,4 @@
     <!-- Espaço compensando a navbar fixa -->
     <div class="h-28"></div>
 
-    <main class="container mx-auto p-4">
+    <main class="container mx-auto flex-grow p-4">
